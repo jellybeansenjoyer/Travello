f431fa9a74ba633cb47444d2612b911c8ff9ce2c
Raj Bagaria <bagariaraj23@gmail.com>
2024-08-17 15:43:57 +0530

    sign In UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,9 +45,9 @@ android {
 
 dependencies {
     //retrofit dependencies
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:okhttp:4.9.0")
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.okhttp)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
